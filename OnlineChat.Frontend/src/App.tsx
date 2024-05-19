3be1652cec85e9023446aa9f4e713a88a6a03af8
SmakleAst <SmakleLover@yandex.ru>
2024-05-19 19:37:21 +0300

    Add chat connection logic

--- OnlineChat.Frontend/src/App.tsx
+++ OnlineChat.Frontend/src/App.tsx
@@ -1,10 +1,26 @@
+import { HubConnectionBuilder } from "@microsoft/signalr"
 import { WaitingRoom } from "./components/WaitingRoom"
 
 
 function App() {
+  const joinChat = async (userName, chatRoom) =>  {
+    const connection = new HubConnectionBuilder()
+      .withUrl("https://localhost:7062/chat")
+      .withAutomaticReconnect()
+      .build();
+
+      try{
+        await connection.start();
+        await connection.invoke("JoinChat", {userName, chatRoom});
+      }
+      catch (error) {
+        console.log(error);
+      }
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <WaitingRoom />
+      <WaitingRoom joinChat={joinChat}/>
     </div>
   )
 }
