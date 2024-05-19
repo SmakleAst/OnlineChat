e54990a7e712329d20f52a952cf9c20a0d557e2b
SmakleAst <SmakleLover@yandex.ru>
2024-05-19 18:50:32 +0300

    Join chat notification

--- OnlineChat.Backend/OnlineChat.Api/Hubs/ChatHub.cs
+++ OnlineChat.Backend/OnlineChat.Api/Hubs/ChatHub.cs
@@ -3,11 +3,20 @@ using OnlineChat.Api.Models;
 
 namespace OnlineChat.Api.Hubs
 {
-    public class ChatHub : Hub
+    public class ChatHub : Hub<IChatClient>
     {
         public async Task JoinChat(UserConnection connection)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
+
+            await Clients
+                .Group(connection.ChatRoom)
+                .ReceiveMessage("Admin", $"{connection.UserName} присоединился к чату");
         }
+    }
+
+    public interface IChatClient
+    {
+        public Task ReceiveMessage(string userName, string message);
     }
 }
