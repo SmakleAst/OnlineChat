70d2588e7d2e980cd7b6b415cd6c12c97dc1a5cb
SmakleAst <SmakleLover@yandex.ru>
2024-05-19 21:50:30 +0300

    Frontend implementation

--- OnlineChat.Backend/OnlineChat.Api/Program.cs
+++ OnlineChat.Backend/OnlineChat.Api/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:3000")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
